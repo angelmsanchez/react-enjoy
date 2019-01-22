21abcf9bd2331d9222ac73488d018d47fe628023
Angel Munoz <angelmsanchez85@gmail.com>
2019-01-22 12:10:57 +0100

    create first components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import logo from '../styles/img/logo.svg';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    );
+  }
+}
---
+++ src/components/Square.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+export default class Square extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
